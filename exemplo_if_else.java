589c4c7cd398505bb9d08e385093e036a8ce0e0b
LeiaJedi <larissa.meirelles@hotmail.com>
2021-03-29 01:24:28 -0300

    oi

---
+++ exemplo_if_else.java
@@ -0,0 +1,10 @@
+class exemplo_if_else{
+    public static void main (String[] args){
+    int a = 5, b = 3, m;
+    if ((a > 0) && (b > 0)) {
+    } else{
+    System.out.println("numero negativo");
+    m=0;
+    }
+    }
+    }
---
+++ exemplo_switch_case.java
@@ -0,0 +1,16 @@
+class exemplo_switch_case {
+        public static void main(String[] args){
+        int a = 5;
+        switch (a){
+        case 5:
+        System.out.println(5);
+        break;
+        case 0:
+        System.out.println(0);
+        break;
+        default:
+        System.out.println("erro");
+        }
+        }
+}
+
